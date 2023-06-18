14ec5f77c0c0bc86cf0ce424c98e4e19145f4a82
swedev99 <swedevops99@gmail.com>
2023-06-18 13:32:38 +0530

    Jenkins pipeline concepts

--- vars/python.groovy
+++ vars/python.groovy
@@ -12,48 +12,45 @@ def call() {
         }
 
 
-
         stages {
 
-            stage('Code Quality'){
-                steps  {
-         //           sh 'ls -l'
-         //           sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.118:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+            stage('Code Quality') {
+                steps {
+                    //           sh 'ls -l'
+                    //           sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.118:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                     sh 'echo Code Quality'
 
                 }
 
             }
 
-            stage('Unit Test Cases'){
-                steps  {
+            stage('Unit Test Cases') {
+                steps {
                     sh 'echo Unit Test Cases'
-                  //  sh 'python3.6 -m unittest'
+                    //  sh 'python3.6 -m unittest'
                 }
 
             }
-            stage('CheckMarx SAST Scan'){
-                steps  {
+            stage('CheckMarx SAST Scan') {
+                steps {
                     sh 'echo checkmarx Scan'
                 }
 
             }
-            stage('CheckMarx SCA Scan'){
-                steps  {
+            stage('CheckMarx SCA Scan') {
+                steps {
                     sh 'echo checkmarx SCA Scan'
                 }
 
             }
-            steps  {
+            steps {
                 sh 'echo $TAG_NAME >VERSION'
                 sh 'zip -r ${component}-${TAG_NAME}.zip *.py *.txt VERSION '
                 sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW } --upload-file ${component}-${TAG_NAME}.zip http://172.31.86.184:8081/repository/${component}/${component}-${TAG_NAME}.zip'
             }
 
         }
-
-
-        }
+    }
 
         post {
             always {
@@ -62,6 +59,7 @@ def call() {
         }
 
     }
+
 
 
 
