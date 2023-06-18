7a58b0497195b14e42f03b4fdf768f379ce06745
swedev99 <swedevops99@gmail.com>
2023-06-18 12:47:03 +0530

    Jenkins pipeline concepts

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -10,9 +10,7 @@ def call() {
         options {
             ansiColor('xterm')
         }
-        environment {
-            NEXUS = credentials('NEXUS')
-        }
+
 
 
         stages {
@@ -53,8 +51,10 @@ def call() {
                 steps  {
                     sh 'npm install'
                     sh 'echo $TAG_NAME >VERSION'
-                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
-                    sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW } --upload-file ${component}-${TAG_NAME}.zip http://172.31.86.184:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *'
+                    //Deleting this file as it is not needed.
+                    sh 'zip -d ${component}-${TAG_NAME}.zip jenkinsfile'
+                    sh 'curl -v -u admin:admin --upload-file ${component}-${TAG_NAME}.zip http://172.31.86.184:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
 
             }
