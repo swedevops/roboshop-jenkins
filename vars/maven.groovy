e092247afea32c33c0df1545ff3e138547111cdb
swedev99 <swedevops99@gmail.com>
2023-06-19 18:49:40 +0530

    Jenkins pipeline concepts

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -60,7 +60,7 @@ def call() {
                 steps  {
                     sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
                     sh 'echo $TAG_NAME >VERSION'
-                    sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schema_dir}'
                     sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.86.184:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
 
