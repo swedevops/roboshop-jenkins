56c81700553ed3ed794d5270fdc917f81afb971a
swedev99 <swedevops99@gmail.com>
2023-07-09 19:58:16 +0530

    Jenkins pipeline concepts

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -61,9 +61,14 @@ def call() {
                     sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'if [ -n "${schema_dir}" ]; then  aws ssm put-parameter --name "${component}.schema.checksum" --type "String" --value "$(md5sum schema/*.sql | awk "{print \\$1}")" --overwrite; fi '
-                    sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schema_dir}'
-                    sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.58:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                   // sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schema_dir}'
+                   // sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.58:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 494970943538.dkr.ecr.us-east-1.amazonaws.com'
+                    sh 'docker build -t 494970943538.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+                    sh 'docker push -t 494970943538.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
                 }
+
+
 
             }
 
