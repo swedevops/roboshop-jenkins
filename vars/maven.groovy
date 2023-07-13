b76d208da974fb4d5f73fb277d52f38c9b07d369
swedev99 <swedevops99@gmail.com>
2023-07-13 15:50:46 +0530

    Jenkins pipeline concepts

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -17,60 +17,60 @@ def call() {
 
 
         stages {
-            stage('Code Compile'){
-                steps  {
+            stage('Code Compile') {
+                steps {
                     sh 'mvn compile'
                 }
 
             }
 
-            stage('Code Quality'){
-                steps  {
-                  //  sh 'ls -l'
-                   // sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.118:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
-                  sh 'echo Code Quality'
+            stage('Code Quality') {
+                steps {
+                    //  sh 'ls -l'
+                    // sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.118:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+                    sh 'echo Code Quality'
                 }
             }
 
-            stage('Unit Test Cases'){
-                steps  {
+            stage('Unit Test Cases') {
+                steps {
                     sh 'echo Unit Test Cases'
                     sh 'mvn test'
                 }
 
             }
-            stage('CheckMarx SAST Scan'){
-                steps  {
+            stage('CheckMarx SAST Scan') {
+                steps {
                     sh 'echo checkmarx Scan'
                 }
 
             }
-            stage('CheckMarx SCA Scan'){
-                steps  {
+            stage('CheckMarx SCA Scan') {
+                steps {
                     sh 'echo checkmarx SCA Scan'
                 }
 
             }
-            stage('Release Application'){
-                when{
-                    expression{
+            stage('Release Application') {
+                when {
+                    expression {
                         env.TAG_NAME ==~ ".*"
                     }
                 }
-                steps  {
+                steps {
                     sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'if [ -n "${schema_dir}" ]; then  aws ssm put-parameter --name "${component}.schema.checksum" --type "String" --value "$(md5sum schema/*.sql | awk "{print \\$1}")" --overwrite; fi '
-                   // sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schema_dir}'
-                   // sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.58:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    // sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schema_dir}'
+                    // sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.58:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                     sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 283181175075.dkr.ecr.us-east-1.amazonaws.com'
                     sh 'docker build -t 283181175075.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
                     sh 'docker push 283181175075.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
                 }
 
 
-
             }
+
 
 
 
